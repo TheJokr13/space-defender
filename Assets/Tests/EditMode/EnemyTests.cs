938cb25014d7d47b7a7793c719f1d5f0d485b0df
TheJokr13 <150007860+TheJokr13@users.noreply.github.com>
2026-03-18 14:53:00 +0100

    [EDIT] Testing new version on advice given by Gemini when shamelessly calling it for help

--- Assets/Tests/EditMode/EnemyTests.cs
+++ Assets/Tests/EditMode/EnemyTests.cs
@@ -11,13 +11,4 @@ public class EnemyTests
         enemy.TakeDamage(50);
         Assert.IsFalse(enemy.IsAlive);
     }
-
-    [Test]
-    public void GetReward_WhenAlreadyDead_ReturnsZero() 
-    {
-        var enemy = new Enemy();
-        Assert.AreEqual(0, enemy.GetReward());
-        enemy.TakeDamage(50);
-        Assert.AreEqual(100, enemy.GetReward());
-    }
 }
